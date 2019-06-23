66f581799e173e913832ba9d2a7d02a7fad6e0c0
danirivas7 <39808909+danirivas7@users.noreply.github.com>
2019-06-23 14:19:11 -0400

    Add files via upload

--- workspace/FLASH/Proyect_settings/Startup_Code/start08_c.d
+++ workspace/FLASH/Proyect_settings/Startup_Code/start08_c.d
@@ -1,6 +1,6 @@
 
 Project_Settings/Startup_Code/start08_c.obj: \
-	C:/Users/mhurtado/workspace/Oscilocopio-EC3882/Project_Headers/../Project_Settings/Startup_Code/start08.c \
+	C:/Users/USER/workspace/Filtro\ FIR/Project_Headers/../Project_Settings/Startup_Code/start08.c \
 	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/start08.h \
 	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/hidef.h \
 	C:/Freescale/CW\ MCU\ v10.6/MCU/lib/hc08c/include/stddef.h \
